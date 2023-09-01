a6c1f187b9dfd11dc384c516e4f67362790b2f1e
Guru Manglam Siddhe <guru@Gurus-MacBook-Air.local>
2023-09-01 10:56:37 +0530

    d

--- src/main/java/academy/learnprogramming/HelloMaven.java
+++ src/main/java/academy/learnprogramming/HelloMaven.java
@@ -1,7 +1,15 @@
 package academy.learnprogramming;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloMaven {
+//    private final static Logger log= LoggerFactory.getLogger(HelloMaven.class);
+@GetMapping("/hello")
     public static void main(String[] args) {
         System.out.println("qwertyuiop ");
+//        log.info("dfh");
+//        log.debug("hello");
     }
 }
